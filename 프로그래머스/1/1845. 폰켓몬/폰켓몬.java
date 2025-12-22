9a5a6a8f189408167e51530f19b45178bc00a795
Kim Dayeon <danhandev@gmail.com>
2025-12-22 21:26:17 +0900

    [level 1] Title: 폰켓몬, Time: 5.94 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,23 +1,18 @@
 // 최대한 많은 종류의 폰켓몬을 포함해서 N/2마리를 선택
-// 중복 제거 k < N/2: k, k >= N/2: N/2
-import java.util.HashMap;
-import java.util.Map;
+// 핵심 중복 제거: k < N/2: k, k >= N/2: N/2
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int solution(int[] nums) {
-        Map<Integer, Integer> poketmon_hash = new HashMap<>();
-        int answer = nums.length / 2; // 최대 고를 수 있는 폰켓몬 종류의 수
-            
-        for (int num: nums) {
-            poketmon_hash.put(num, poketmon_hash.getOrDefault(num, 0) + 1);
+        Set<Integer> types = new HashSet<>();
+        
+        for (int num : nums) {
+            types.add(num);
         }
         
-        int numberOfKeys = poketmon_hash.size();
+        int maxSelectable = nums.length / 2;
         
-        if (numberOfKeys < answer) {
-            return numberOfKeys;
-        } else {
-            return answer;
-        }
+        return Math.min(types.size(), maxSelectable);
     }
 }
