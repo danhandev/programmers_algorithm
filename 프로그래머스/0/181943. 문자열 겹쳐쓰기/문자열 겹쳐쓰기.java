aee4857917c48cb3a34f3ca82dba42a1c1a3fb06
Kim Dayeon <danhandev@gmail.com>
2026-04-11 23:19:12 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 15.28 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,20 +1,8 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
         
         int length = overwrite_string.length();
         
-        for (int i = 0; i < my_string.length(); i++) {
-            if (i < s) {
-                sb.append(my_string.charAt(i));
-            } else if (i < s + length) {
-                sb.append(overwrite_string.charAt(i - s)); 
-            } else {
-                sb.append(my_string.charAt(i));
-            }
-        }
-        
-        return sb.toString();
+        return my_string.substring(0,s) + overwrite_string + my_string.substring(s + length);
     }
 }
