91a24039e5524d711e7ed08cecd8a41d1a2af557
Kim Dayeon <danhandev@gmail.com>
2026-04-18 19:18:27 +0900

    [level 0] Title: 접미사 배열, Time: 0.52 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            result.add(my_string.substring(i, my_string.length()));
+        }
+        
+        Collections.sort(result);
+        
+        return result.toArray(new String[0]);
+    }
+}
