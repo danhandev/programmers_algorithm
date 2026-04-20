25002d15eca77bb30d668cc53d00061fc5bd3e5a
Kim Dayeon <danhandev@gmail.com>
2026-04-20 22:22:22 +0900

    [level 0] Title: 글자 지우기, Time: 0.29 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            boolean flag = true;
+            
+            for (int indice : indices) {
+                if (i == indice) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
