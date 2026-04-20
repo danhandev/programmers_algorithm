1b09c1b665dd52182c3126715f595e84d3f609e3
Kim Dayeon <danhandev@gmail.com>
2026-04-20 19:41:47 +0900

    [level 0] Title: 세로 읽기, Time: 0.20 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        return new String(sb);
+    }
+}
