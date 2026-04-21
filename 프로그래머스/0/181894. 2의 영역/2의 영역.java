97870707be58e2aaf360c3a3497fd35c3627c7b9
Kim Dayeon <danhandev@gmail.com>
2026-04-21 18:21:13 +0900

    [level 0] Title: 2의 영역, Time: 3.29 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int startIndex = -1;
+        int endIndex = -1;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                if (startIndex == -1) {
+                    startIndex = i;
+                }
+                
+                endIndex = i;
+            }
+        }
+        
+        
+        if (startIndex == -1) {
+            return new int[]{-1};
+        }
+                
+        return Arrays.copyOfRange(arr, startIndex, endIndex + 1);
+    }
+}
