1c3a239296cf755d96ae2654254c5daf67d1adc0
Kim Dayeon <danhandev@gmail.com>
2026-04-22 23:35:17 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.07 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int odd_sum = 0;
+        int even_sum = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                even_sum += num_list[i];
+            } else {
+                odd_sum += num_list[i];
+            }
+        }
+        
+        return Math.max(odd_sum, even_sum);
+    }
+}
