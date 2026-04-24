38781e753e617280b46412f05cd56abf5049db10
Kim Dayeon <danhandev@gmail.com>
2026-04-24 19:50:49 +0900

    [level 0] Title: ad 제거하기, Time: 0.82 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,16 @@
+import java.util.stream.*;
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> result = new ArrayList<>();
+        
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+                result.add(str);
+            }
+        }
+        
+        return result.toArray(new String[0]);
+    }
+}
