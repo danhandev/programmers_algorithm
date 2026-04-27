d90b9a5b64c6e13d9a165a8f654cbce49e906067
Kim Dayeon <danhandev@gmail.com>
2026-04-27 23:00:51 +0900

    [level 2] Title: 가장 큰 수, Time: 203.87 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,32 +1,25 @@
-import java.util.PriorityQueue;
-import java.util.Comparator;
+import java.util.*;
 
-/*
-* 먼저 떠올린 아이디어 = 각 number에서 가장 큰 자리수 숫자가 클 수록 먼저 배치해야함
-* -> 반례 [121, 12] 12+121 = 12121 vs 121+12 = 12112 → 12가 먼저 와야함
-* -> 문자열 두 수를 붙여본 결과(ab vs ba)로 비교해야함
-*/
 class Solution {
     public String solution(int[] numbers) {
-        // 두 수를 붙혔을 때 큰 수
-        Comparator<String> cmp = (a, b) -> (b+a).compareTo(a+b);
-        PriorityQueue<String> pq = new PriorityQueue<>(cmp);
+        String[] array = new String[numbers.length];
         
-        // PriorityQueue에 삽입
-        for (int num : numbers) {
-            pq.offer(String.valueOf(num));
+        for (int i = 0; i < numbers.length; i++) {
+            array[i] = String.valueOf(numbers[i]);
         }
         
-        // 모든 입력이 0일 경우 처리
-        if (pq.peek().equals("0")) return "0";
+        Arrays.sort(array, (a, b) -> (b + a).compareTo(a + b));
         
-        // 문자열 병합
+        // 정렬 후 가장 큰 숫자가 '0'이라면, 모든 숫자가 0이므로 "0"을 리턴
+        if (array[0].equals("0")) {
+            return "0";
+        }
+        
         StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()) {
-            sb.append(pq.poll());
+        for (int i = 0; i < numbers.length; i++) {
+            sb.append(array[i]);
         }
         
         return sb.toString();
     }
 }
-
