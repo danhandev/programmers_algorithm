cd65e13e294f626dbef9bd3a16b210c75fa85c64
Kim Dayeon <danhandev@gmail.com>
2026-05-13 16:54:24 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.44 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < rank.length; i++) {
+            if (attendance[i]) {
+                list.add(i);
+            }
+        }
+        
+        Collections.sort(list, (a, b) -> rank[a] - rank[b]);
+        
+        int a = list.get(0);
+        int b = list.get(1);
+        int c = list.get(2);
+        
+        return 10000 * a + 100 * b + c;
+    }
+}
