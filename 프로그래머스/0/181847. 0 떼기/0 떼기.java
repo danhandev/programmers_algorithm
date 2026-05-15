f7d1ec18265d3505a5e6828ac837ba97b09d8581
Kim Dayeon <danhandev@gmail.com>
2026-05-15 15:20:20 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,14 +1,16 @@
 class Solution {
     public String solution(String n_str) {
-        int strStartIndex = -1;
-        
+        // 1. n_str의 길이만큼 순회하며 0이 아닌 문자의 위치를 찾습니다.
         for (int i = 0; i < n_str.length(); i++) {
-            if ('0' != n_str.charAt(i)) {
-                strStartIndex = i;
-                break;
+            // 2. 처음으로 '0'이 아닌 문자가 등장하면
+            if (n_str.charAt(i) != '0') {
+                // 3. 그 위치(i)부터 끝까지 바로 잘라서 반환합니다.
+                // 별도의 변수 저장과 break 없이 바로 return이 가능합니다.
+                return n_str.substring(i);
             }
         }
         
-        return n_str.substring(strStartIndex);
+        // 제한사항에 의해 도달할 일은 없으나, 컴파일러를 위해 기본 반환값을 둡니다.
+        return n_str;
     }
 }
