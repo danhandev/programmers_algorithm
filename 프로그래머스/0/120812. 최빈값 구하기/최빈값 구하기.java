f8f1ec2a45a77c3ec92e2c7825295ba6fe1771f1
Kim Dayeon <danhandev@gmail.com>
2026-05-19 16:44:32 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.54 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,39 +1,27 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        // 1. 숫자의 등장 빈도를 저장할 Map을 생성합니다. (Key: 숫자, Value: 등장 횟수)
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        int maxCount = 0;
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
         
-        // 2. 배열을 순회하며 각 숫자의 빈도수를 누적합니다.
-        // 이전에 배웠던 getOrDefault를 활용하여 깔끔하게 처리합니다.
-        for (int num : array) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-        
-        int maxFrequency = 0; // 가장 높은 빈도수
-        int mode = -1;        // 최빈값 (기본값 -1)
-        boolean isDuplicate = false; // 최빈값이 중복되는지 여부
-        
-        // 3. 빈도수 맵을 순회하며 최빈값을 찾습니다.
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            int num = entry.getKey();
-            int frequency = entry.getValue();
+        for (int number : array) {
+            // 1. 실시간으로 빈도수를 계산합니다.
+            int count = map.getOrDefault(number, 0) + 1;
+            map.put(number, count); // 맵에 먼저 업데이트를 반영합니다.
             
-            // 현재 숫자의 빈도수가 기존 최댓값보다 크다면 새로운 최빈값으로 갱신합니다.
-            if (frequency > maxFrequency) {
-                maxFrequency = frequency;
-                mode = num;
-                isDuplicate = false; // 새로운 최댓값이 나왔으므로 중복 상태를 해제합니다.
+            // 2. 단독 1등이 나타난 경우
+            if (count > maxCount) {
+                maxCount = count;
+                answer = number;
             } 
-            // 현재 숫자의 빈도수가 기존 최댓값과 같다면 중복된 최빈값입니다.
-            else if (frequency == maxFrequency) {
-                isDuplicate = true;
+            // 3. 공동 1등(최빈값 중복)이 발생한 경우
+            else if (count == maxCount) {
+                answer = -1;
             }
         }
         
-        // 4. 최빈값이 여러 개면 -1을, 하나뿐이면 해당 최빈값을 반환합니다.
-        return isDuplicate ? -1 : mode;
+        return answer;
     }
 }
