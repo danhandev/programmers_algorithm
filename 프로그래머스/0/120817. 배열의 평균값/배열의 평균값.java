9958e2ca8e6e950cd2e7b71592b198c0861270f5
Kim Dayeon <danhandev@gmail.com>
2026-05-20 12:41:12 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] numbers) {
+        
+        int sum = 0;
+        
+        for (int n : numbers) {
+            sum += n;
+        }
+        
+        return (double) sum / numbers.length;
+    }
+}
