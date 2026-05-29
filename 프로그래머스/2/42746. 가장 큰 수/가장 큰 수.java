e4d39784d1973e344ee63876b5340d4c3bfaaada
Kim Dayeon <danhandev@gmail.com>
2026-05-29 12:36:21 +0900

    [level 2] Title: 가장 큰 수, Time: 102.92 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,22 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] array = new String[numbers.length];
-        
+        String[] strNumbers = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            array[i] = String.valueOf(numbers[i]);
+            strNumbers[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(array, (a, b) -> (b + a).compareTo(a + b));
+        Arrays.sort(strNumbers, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         
-        // 정렬 후 가장 큰 숫자가 '0'이라면, 모든 숫자가 0이므로 "0"을 리턴
-        if (array[0].equals("0")) {
+        if (strNumbers[0].equals("0")) {
             return "0";
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < numbers.length; i++) {
-            sb.append(array[i]);
+        for (String str : strNumbers) {
+            sb.append(str);
         }
         
         return sb.toString();
