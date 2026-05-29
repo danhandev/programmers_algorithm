dcacdfe58fdc2a108d8e073a751310e420d89c65
Kim Dayeon <danhandev@gmail.com>
2026-05-29 09:32:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        StringBuilder sb = new StringBuilder(my_string);
+        char num1Char = sb.charAt(num1);
+        sb.setCharAt(num1, sb.charAt(num2));
+        sb.setCharAt(num2, num1Char);
+        return sb.toString();
+    }
+}
