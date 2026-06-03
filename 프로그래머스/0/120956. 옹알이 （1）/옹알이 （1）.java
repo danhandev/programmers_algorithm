f1fc410e366f1b18dce2609fe76ebd0d2205bedf
Kim Dayeon <danhandev@gmail.com>
2026-06-03 19:15:13 +0900

    [level 0] Title: 옹알이 (1), Time: 5.81 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,10 +2,7 @@ class Solution {
     public int solution(String[] babbling) {
         int count = 0;
         for (String b : babbling) {
-            b = b.replace("aya", " ");
-            b = b.replace("ye", " ");
-            b = b.replace("woo", " ");
-            b = b.replace("ma", " ");
+            b = b.replaceAll("(aya|ye|woo|ma)+", " ");
             b = b.replace(" ", "");
             if (b.isEmpty()) count++;
             System.out.println(b);
