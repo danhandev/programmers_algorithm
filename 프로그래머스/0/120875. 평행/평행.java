a70fb57068f74cada8c2622fa12c8b8b9bd0a894
Kim Dayeon <danhandev@gmail.com>
2026-06-03 17:32:26 +0900

    [level 0] Title: 평행, Time: 0.93 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -6,13 +6,10 @@ class Solution {
             if (a[0] == b[0]) return a[1] - b[1];
             return a[0] - b[0];
         });
-        for (int i = 0; i < dots.length; i++) {
-            System.out.println(dots[i][0] + " " + dots[i][1]);
-        }
+        
         int dot1dot2 = Math.abs(10000 * (dots[0][0] - dots[1][0]) / (dots[0][1] - dots[1][1]));
         int dot3dot4 = Math.abs(10000 * (dots[2][0] - dots[3][0]) / (dots[2][1] - dots[3][1]));
         
-        System.out.println(dot1dot2 + " " + dot3dot4);
         return (dot1dot2 == dot3dot4) ? 1 : 0;
     }
 }
