f7ad37504addb52c277c4b830e399039cd225763
Kim Dayeon <danhandev@gmail.com>
2026-06-04 09:20:45 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int chicken) {
+        int coupon = 0;
+        while (chicken >= 10) {
+            coupon += chicken / 10;
+            chicken = chicken % 10 + chicken / 10;
+        }
+        return coupon;
+    }
+}
