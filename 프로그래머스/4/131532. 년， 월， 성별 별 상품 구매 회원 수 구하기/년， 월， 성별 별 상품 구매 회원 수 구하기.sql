82b2738548bbae6ac5baba6eeffdf3cf340ca167
Kim Dayeon <danhandev@gmail.com>
2026-06-29 10:45:20 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,11 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/131532
+#  회원수 - 그룹별(년, 월, 성별) - 오름차순 / 성별 null이면 제외
 SELECT
-    YEAR(SALES_DATE) AS YEAR,
-    MONTH(SALES_DATE) AS MONTH,
-    GENDER,
-    COUNT(DISTINCT(o.USER_ID)) AS USERS
-FROM ONLINE_SALE o
-JOIN USER_INFO i
-ON o.USER_ID = i.USER_ID
-WHERE i.GENDER is not null
-GROUP BY YEAR(SALES_DATE), MONTH(SALES_DATE), GENDER
-ORDER BY YEAR ASC, MONTH ASC, GENDER ASC
+    YEAR(o.SALES_DATE) AS YEAR,
+    MONTH(o.SALES_DATE) AS MONTH,
+    u.gender AS GENDER,
+    COUNT(DISTINCT u.user_id) AS USERS
+FROM USER_INFO u
+JOIN ONLINE_SALE o ON u.user_id = o.user_id
+WHERE u.gender IS NOT NULL
+GROUP BY YEAR, MONTH, u.gender
+ORDER BY year ASC, month ASC, gender ASC;
+
+# 문제를 제대로 안 읽어서 sales_date가 아니라 joined 기준으로 group by를 하고 있었음. 문제만 읽고 바로 풀지 않고 예시까지 시뮬레이션 해보는게 중요함
+# user 중복 제거를 위해 DISTINCT 키워드도 사용함.
