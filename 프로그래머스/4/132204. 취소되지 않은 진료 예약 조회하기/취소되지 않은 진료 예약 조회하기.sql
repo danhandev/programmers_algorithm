df973c9e6bccd2f2f584fd3594758859b24ecf8c
Kim Dayeon <danhandev@gmail.com>
2026-08-27 09:36:14 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,13 +1,17 @@
+# 2022년 4월 13일 기준 -> 취소되지 않은
+# 흉부외과(CS) 진료 예약
+# 진료예약일시를 기준으로 오름차순
 SELECT
     a.APNT_NO,
-    p.PT_NAME,
-    a.PT_NO,
-    d.MCDP_CD,
+    p.PT_NAME, 
+    p.PT_NO,
+    a.MCDP_CD,
     d.DR_NAME,
     a.APNT_YMD
 FROM APPOINTMENT a
-JOIN PATIENT p ON a.PT_NO = p.PT_NO
-JOIN DOCTOR d ON a.MDDR_ID = d.DR_ID
-WHERE DATE_FORMAT(APNT_YMD, '%Y-%m-%d') = '2022-04-13' 
-    AND APNT_CNCL_YN = 'N'
+JOIN DOCTOR d ON d.DR_ID = a.MDDR_ID
+JOIN PATIENT p ON p.PT_NO = a.PT_NO
+WHERE a.MCDP_CD = "CS"
+    AND a.APNT_CNCL_YN = "N"
+    AND a.APNT_YMD >= '2022-04-13' AND a.APNT_YMD < '2022-04-14'
 ORDER BY APNT_YMD ASC;
