3cbb10811bdac8578183e299430a627b5e5c8b18
Kim Dayeon <danhandev@gmail.com>
2026-08-27 10:44:24 +0900

    [level 2] Title: 전화번호 목록, Time: 212.54 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,21 +1,12 @@
+// 접두어 - 앞에서부터 자르기
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
-        for (String p : phone_book) {
-            set.add(p);
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
         }
-        
-        for (String p : phone_book) {
-            for (int i = 1; i < p.length(); i++) {
-                String prefix = p.substring(0, i);
-                if (set.contains(prefix)) {
-                    return false;
-                }
-            }
-        }
-        
         return true;
     }
 }
