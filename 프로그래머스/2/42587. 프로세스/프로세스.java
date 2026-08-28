16bfe7d0c86288df4de94235155beea24fad4012
Kim Dayeon <danhandev@gmail.com>
2026-08-28 09:19:17 +0900

    [level 2] Title: 프로세스, Time: 1.55 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,43 +1,31 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> queue = new LinkedList<>(); 
-        // queue 원소: [우선순위, 원래 인덱스]
-
+        Deque<int[]> q = new ArrayDeque<>();
         for (int i = 0; i < priorities.length; i++) {
-            queue.offer(new int[]{priorities[i], i});
+            q.offer(new int[]{priorities[i], i});
         }
-
-        int executionOrder = 0;
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int priority = current[0];
-            int index = current[1];
-
-            // 큐에 현재 priority보다 높은 priority가 존재하는지 확인
-            boolean hasHigherPriority = false;
-            for (int[] q : queue) {
-                if (q[0] > priority) {
-                    hasHigherPriority = true;
+        
+        int execute_count = 0;
+        while (true) {
+            int[] current = q.poll();
+            boolean hasHigher = false;
+            for (int[] other : q) {
+                if (other[0] > current[0]) {
+                    hasHigher = true;
                     break;
                 }
             }
-
-            if (hasHigherPriority) {
-                // 뒤로 보냄
-                queue.offer(current);
+            
+            if (hasHigher) {
+                q.offer(current);
             } else {
-                // 실행
-                executionOrder++;
-                if (index == location) {
-                    return executionOrder;
+                execute_count++; 
+                if (current[1] == location) {
+                    return execute_count;
                 }
             }
         }
-
-        return executionOrder;
     }
 }
